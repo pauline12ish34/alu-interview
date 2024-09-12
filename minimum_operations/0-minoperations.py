0a8784e3bfd41a6411256c42c3d0f17490e440a3
pauline12ish34 <paulineishimwe12@gmail.com>
2024-09-12 13:16:06 +0200

    mini operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""mini operations"""
+
+# function for mini operation
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    ops = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            ops += divisor
+            n = n // divisor
+        else:
+            divisor += 1
+
+    return ops
