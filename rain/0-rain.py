3fe24be45731855d86019ba19368ba5ffa1fc652
pauline12ish34 <paulineishimwe12@gmail.com>
2024-09-28 01:10:46 +0200

    added pep8

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""calculates how much rain will be trapped after it rains
+import pep8
+"""trapped rain
 """
 
 def rain(walls):
-    """calculates how much rain will be trapped after it rains
-    """
+    """calculating rain """
     if not walls or len(walls) < 3:
         return 0
 
