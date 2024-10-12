3c3fc0ce4ab295f1b063d349d09d2c2ec5e1215e
pauline12ish34 <paulineishimwe12@gmail.com>
2024-10-12 21:02:37 +0200

    resolved validation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -4,8 +4,7 @@
 
 
 def pascal_triangle(n):
-    """function returns a list of lists of integers representing the Pascal’s triangle
-    """
+    """function returns a list of lists of integers representing the Pascal’s triangle"""
     if n <= 0:
         return []
     triangle = [[1]]
